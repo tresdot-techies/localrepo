1c6dc7a93ae792ea13527411286ba49ab77eb565
tresdot-techies <mansoor0mughal@gmail.com>
2023-01-10 22:13:37 +0500

    android and ios support add

---
+++ jang_angular_project/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.jang_angular.app',
+  appName: 'jang_angular_project',
+  webDir: 'dist/jang_angular_project',
+  bundledWebRuntime: false
+};
+
+export default config;
